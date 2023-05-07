ee646db5361c99d3f3c309dbf68c547713da2d28
MoumitaGuha <send2moumitaguha91@gmail.com>
2023-05-07 23:18:02 +0530

    test commit

--- app/src/main/java/com/example/newtest/MainActivity.kt
+++ app/src/main/java/com/example/newtest/MainActivity.kt
@@ -24,6 +24,8 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
+        //comment line
+
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
